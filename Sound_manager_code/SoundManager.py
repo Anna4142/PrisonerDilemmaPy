770571b2d998fb0cd20b05dc0899c3877c907ba0
Anna4142 <103633351+Anna4142@users.noreply.github.com>
2023-12-20 11:12:00 +0200

    Revert "change sound file location"

--- Sound_manager_code/SoundManager.py
+++ Sound_manager_code/SoundManager.py
@@ -1,3 +1,14 @@
+# Micky: I changed the file name, just to maintain consistency. Some naming conventions is good. It helps read the code. The occasional
+#         use of '_" catches my attention and always think "why". What is special about this name compared to all the others?
+#         what does this suppose to indicate?
+# Micky: I also recommend using relative (e.g. starting from the python working directory, go one up and then to the Sounds/ ) file names
+#         rather then absolute. This way you can move the project from one comuter to another. copy Ben's sound files to your project library
+# Micky:  Also do not configure the sound files from the experiment manager. It defeats the purpose of having a class. Hide the details here.
+# Micky;  I switched to the winsound module, becuase its simple and pure python. pygame is a lot havier and requires Visual C to be installed.
+#         Any reason you chose pygame package? One caveat, windound requires wav files. Is it ok?
+# Micky:  Also, please note that I wrote it as a module rather than a class. When you need one instance of a class and it does not require
+#         any "self" variables, this is the preferred (I think) python solution. In C++ you would have used static classes.
+
 import winsound
 from enum import Enum
 
@@ -7,6 +18,6 @@ class Sounds (Enum):
 
 def Play(sound):
     if sound == Sounds.Start:
-        winsound.PlaySound("./../../Sounds/beep.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
+        winsound.PlaySound("C:/Users/EngelHardBlab.MEDICINE/Desktop/experimentfolder/sounds/beep.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
     elif sound == Sounds.Abort:
-        winsound.PlaySound("./../../Sounds/ping.wav", winsound.SND_FILENAME |winsound.SND_ASYNC)
+        winsound.PlaySound("C:/Users/EngelHardBlab.MEDICINE/Desktop/experimentfolder/sounds/ping.wav", winsound.SND_FILENAME |winsound.SND_ASYNC)
--- Sound_manager_code/SoundManagerTest.py
+++ Sound_manager_code/SoundManagerTest.py
@@ -1,11 +1,7 @@
-import os
-import time
 from SoundManager import *
+import time
 
-print(os.getcwd())
 Play(Sounds.Abort)
 time.sleep(2)
 Play(Sounds.Start)
 time.sleep(5)
-Play(Sounds.Abort)
-time.sleep(2)

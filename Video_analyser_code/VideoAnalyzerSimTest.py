e9fa8d49a973474837ff28c2000cfe74c4b23983
MickyHoltzman <micky.holtzman@gmail.com>
2023-12-20 15:09:22 +0200

    Reintroducing Video Analyzer Sim
    MouseMonitor1 renamed to MouseMonitor
    added VideoAnayzerSim (and its dedicated test program). This module simulates the mouse locations based on the arrow keys.

---
+++ Video_analyser_code/VideoAnalyzerSimTest.py
@@ -0,0 +1,19 @@
+from VideoAnalyzerSim import Video_Analyzer
+from MouseMonitor import MouseMonitor
+import time
+
+
+# main code for VideoAnalyzerSim unit test
+print("test program for VideoAnalyzerStub")
+videoAnalyzer = Video_Analyzer()
+mouse1 = MouseMonitor(videoAnalyzer, 1)
+mouse2 = MouseMonitor(videoAnalyzer, 2)
+
+for i in range(10):
+    locations = videoAnalyzer.process_single_frame()
+    print(locations)
+    print(mouse1.get_mouse_location(locations))
+    print(mouse2.get_mouse_location(locations))
+    time.sleep(5)
+
+print ("program terminated")
--- modelling_opponent/MouseMonitor.py
+++ modelling_opponent/MouseMonitor.py
@@ -1,18 +1,14 @@
-
 from Video_analyser_code.locations import Locations
 
 class MouseMonitor:
-    def __init__(self, video,mouse_id):
+    def __init__(self, video, mouse_id):
         self.mouse_id = mouse_id
-        self.video_analyser=video
+        self.video_analyser = video
 
-    def get_mouse_location(self, zones_list,currentstate):
+    def get_mouse_location(self, zones_list, current_state):
         # Split the zones based on mouse_id
-
-        print("ZONES LIST",zones_list)
         mouse_zones = zones_list[(self.mouse_id - 1) * 3 : self.mouse_id * 3]
-        print(self.mouse_id)
-        print(mouse_zones)
+
         # Interpret the zones list to return the corresponding location
         location = Locations.Unknown
         if mouse_zones[0] == 1:
@@ -21,7 +17,6 @@ class MouseMonitor:
             location = Locations.Center
         elif mouse_zones[2] == 1:
             location = Locations.Defect
-        print("location",location)
         return location
 
 

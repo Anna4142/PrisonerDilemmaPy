09fd314fbda72772cc4bd9824f1d083587f01e09
Anna4142 <anushka.deshpande2300@gmail.com>
2023-12-31 14:32:40 +0000

    Merge remote-tracking branch 'origin/with_mickys_changes' into with_mickys_changes

--- Video_analyser_code/VideoAnalyser.py
+++ Video_analyser_code/VideoAnalyser.py
@@ -75,47 +75,37 @@ class Video_Analyzer:
     def define_regions(self):
         # Define the regions of interest (ROI) for each mouse and their specific zones
         regions = {
-            'm1_c': [(500, 110), (535, 180)],  # Mouse 1 Cooperate Zone (Top Right)
-            'm1_cen': [(610, 280), (680, 350)],  # Mouse 1 Center Zone (Center Right)
-            'm1_d': [(500, 400), (535, 470)],  # Adjusted Mouse 1 Defect Zone (Bottom Right)
+            'm1_c': [(455, 120), (490, 190)],  # Mouse 2 Cooperate Zone (Top Left)
+            'm1_cen': [(330, 280), (400, 350)],  # Mouse 2 Center Zone (Center Left)
+            'm1_d': [(455, 420), (490, 490)],  # Mouse 2 Defect Zone (Bottom Left)
+            'm2_c': [(510, 120), (545, 190)],  # Mouse 1 Cooperate Zone (Top Right)
+            'm2_cen': [(610, 280), (680, 350)],  # Mouse 1 Center Zone (Center Right)
+            'm2_d': [(510, 420), (545, 490)],  # Adjusted Mouse 1 Defect Zone (Bottom Right)
 
-            'm2_c': [(455, 110), (490, 180)],  # Mouse 2 Cooperate Zone (Top Left)
-            'm2_cen': [(330, 280), (400, 350)],  # Mouse 2 Center Zone (Center Left)
-            'm2_d': [(455, 400), (490, 470)],  # Mouse 2 Defect Zone (Bottom Left)
-            'experimenter_start_zone': [(460, 280), (540, 350)]
+
+            #'experimenter_start_zone': [(460, 280), (540, 350)]
         }
         return regions
 
-    """""
-    def define_thresholds(self):
-        # Define the thresholds for each region
-        thresholds = {
-            'r1': 90000,
-            'r2': 90000,
-            'r3': 90000,
-            'r4': 90000,
-            'r5': 90000,
-            'r6': 90000,
-        }
-        return thresholds
-        """
+
 
     def define_thresholds(self):
         # Define the thresholds for each region
-        thresholds = {
-            'm1_c': 39500,  # Threshold for Mouse 1 Cooperate Zone
-            'm1_cen': 117000,  # Threshold for Mouse 1 Center Zone
-            'm1_d': 110000,  # Threshold for Mouse 1 Defect Zone
-            'm2_c': 35500,  # Threshold for Mouse 2 Cooperate Zone
-            'm2_cen': 300000,  # Threshold for Mouse 2 Center Zone
-            'm2_d': 140000,  # Threshold for Mouse 2 Defect Zone
-            'experimenter_start_zone': 350000  #Threshold for expermenter start zone
+        self.thresholds = {
+            'm1_c': 52000,  # Threshold for Mouse 2 Cooperate Zone
+            'm1_cen': 290000,  # Threshold for Mouse 2 Center Zone
+            'm1_d': 120000,  # Threshold for Mouse 2 Defect Zone
+            'm2_c': 39500,  # Threshold for Mouse 1 Cooperate Zone
+            'm2_cen': 117000,  # Threshold for Mouse 1 Center Zone
+            'm2_d': 98941,  # Threshold for Mouse 1 Defect Zone
+
+            #'experimenter_start_zone': 350000  #Threshold for expermenter start zone
         }
         #{'m1_d': 106569, 'm1_cen': 2632, 'm1_c': 256415, 'm2_d': 112012, 'm2_cen': 505708, 'm2_c': 234711}
-        return thresholds
+        return self.thresholds
 
     def check_zones(self, frame):
-        zone_activation = [0] * 7  # [0, 0, 0, 0, 0, 0]
+        zone_activation = [0] * 6 # [0, 0, 0, 0, 0, 0]
 
         for idx, region_key in enumerate(self.regions):
             (y1, x1), (y2, x2) = self.regions[region_key]
@@ -123,9 +113,10 @@ class Video_Analyzer:
             region_pixels = frame[y1:y2, x1:x2]
             sum_of_pixels = np.sum(frame[y1:y2, x1:x2])
             self.pixel_sums[region_key] = sum_of_pixels  # Update the class attribute
-            print(f"{region_key}: Sum of pixels = {sum_of_pixels}, Region shape = {region_pixels.shape}")
+            #print(f"{region_key}: Sum of pixels = {sum_of_pixels}, Region shape = {region_pixels.shape}")
 
             if sum_of_pixels <= self.thresholds[region_key]:
+
                 zone_activation[idx] = 1
         #print("zone activation",zone_activation)
         return zone_activation

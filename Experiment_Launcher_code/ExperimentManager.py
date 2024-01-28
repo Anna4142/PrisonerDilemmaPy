76fe1e4d3f8cd7cb182867a3624c72221545241e
Anna4142 <anushka.deshpande2300@gmail.com>
2024-01-28 09:59:13 +0000

    Merge pull request #66 from Anna4142/changing_reward_delivery_values_post_caliberation
    Changing reward delivery values post caliberation

--- Experiment_Launcher_code/ExperimentManager.py
+++ Experiment_Launcher_code/ExperimentManager.py
@@ -1,52 +1,27 @@
-# The first group of import statements control the simulated vs real HW environments
 
-from Video_analyser_code.VideoAnalyser import Video_Analyzer
-#from Video_analyser_code.VideoAnalyzerStub import Video_Analyzer
-#from Video_analyser_code.VideoAnalyzerSim import Video_Analyzer
-
-from Arduino_related_code.ArduinoDigital import ArduinoDigital
-#from Arduino_related_code.ArduinoDigitalSim import ArduinoDigital
-
-# The following are configuration independent imports
 from Sound_manager_code.SoundManager import Play, Sounds
-from modelling_opponent.MouseMonitor import MouseMonitor
 from modelling_opponent.MouseMonitor import Locations
-from modelling_opponent.FixedStrategyPrisoner import FixedStrategyPrisoner
-#from modelling_opponent.Simulated_learner import Simulated_mouse
 from State_manager_code.StateManager import StateManager
 from State_manager_code.StateManager import States
 from State_manager_code.StateManager import Events
-
 from Data_analysis.logger import TrialLogger
 from Data_analysis.event_logger import EventLogger
 from Data_analysis.DataAnalysisScript import DataAnalyzer
 from modelling_opponent.OpponentType import OpponentType
-
 from Reward_manager.RewardManager import RewardManager
 import time
-
-
 from pynput.keyboard import Key, Listener
 from threading import Lock
+
+
 class ExperimentManager:
-    def __init__(self, comport,mouse_id,opponenttype):
+    def __init__(self, video_analyzer, reward_manager):
         # initialize software components
-        self.reward_manager = RewardManager(comport)
-        self.opponent_path=opponenttype
-        self.mouse_number=mouse_id
-        if opponenttype == OpponentType.MOUSE_MOUSE:
-
-            self.opponent_path ="MOUSE_MOUSE"
-        elif opponenttype == OpponentType.MOUSE_COMPUTER:
-
-            self.opponent_path = "MOUSE_COMPUTER"
-        else:
-
-            self.opponent_path = "COMPUTER_COMPUTER"
-
-        self.videoAnalyser = Video_Analyzer(self.mouse_number,self.opponent_path)
+        self.reward_manager = reward_manager
+        self.videoAnalyser = video_analyzer
         self.stateManager = StateManager()
         self.trial_logger = TrialLogger()
+
         self.event_logger = EventLogger()
         #self.data_analyzer = DataAnalyzer(self.trial_logger)
 
@@ -55,11 +30,11 @@ class ExperimentManager:
         self.opponent_type = ""  ##for the logger
 
         # Set default reward and punishment times
-        self.reward_time = 0.02
+        self.reward_time = 0.2
         self.sucker_time = 0
-        self.temptation_time = 0.04
-        self.punishment_time = 0.01
-        self.center_reward_time = 0.09
+        self.temptation_time = 0.4
+        self.punishment_time = 0.1
+        self.center_reward_time = 0.02
 
         # initialize experiment control variables
         self.numcompletedtrial = 0
@@ -92,18 +67,19 @@ class ExperimentManager:
 
     def on_release(self, key):
         print(f"Key released: {key}")
-
+    """""
     def start_state_timer(self, state):
-        """ Start the timer for a given state """
+      
         self.state_start_times[state] = time.time()
 
     def get_state_duration(self, state):
-        """ Calculate the duration spent in a given state """
+       
         return time.time() - self.state_start_times.get(state, time.time())
+    """
     def StateActivity(self, state, mouse1, mouse2):
         if state is not None:
-            duration = self.get_state_duration(state)
-            self.event_logger.log_event_data(self.numcompletedtrial,state,duration)
+
+            self.event_logger.log_event_data(self.numcompletedtrial,state,time.time())
 
         if state == States.Start:
 
@@ -115,7 +91,7 @@ class ExperimentManager:
         #    pass
 
         elif state == States.CenterReward:
-
+            print("in cen reward dec time",self.time_to_make_decision)
             self.time_to_make_decision = 0
             self.time_to_return_to_center = 0
 
@@ -126,14 +102,17 @@ class ExperimentManager:
             mouse1.DeliverReward(Locations.Center, self.center_reward_time)
             mouse2.DeliverReward(Locations.Center, self.center_reward_time)
 
-            self.time_start = time.time()
+
 
 
         elif state == States.TrialStarted:
             Play(Sounds.Start)
+            self.time_start = time.time()
+
             if self.numcompletedtrial > 0:
 
-                    self.time_to_return_to_center = time.time() - self.time_to_make_decision- self.time_start
+                    self.time_to_return_to_center = time.time() - self.start_return_time
+            self.start_return_time=0
             mouse1.NewTrial()
             mouse2.NewTrial()
 
@@ -141,8 +120,8 @@ class ExperimentManager:
             # Actions for M1CM2C state
             self.mouse_choice = "C"
             self.opponent_choice = "C"
-            self.mouse_reward = "0.03"
-            self.opponent_reward = "0.03"
+            self.mouse_reward = "0.012"
+            self.opponent_reward = "0.012"
             self.mouse_center_reward="0.0"
             self.opponent_center_reward = "0.0"
             self.cc_cnt += 1
@@ -150,24 +129,30 @@ class ExperimentManager:
             mouse2.DeliverReward(Locations.Cooperate, self.reward_time)
             self.time_to_make_decision = time.time() - self.time_start
 
+            self.numcompletedtrial += 1
+            self.start_return_time = time.time()
+
         elif state == States.M1CM2D:
             # Actions for M1CDM2D state
             self.mouse_choice = "C"
             self.opponent_choice = "D"
             self.mouse_reward = "0"
-            self.opponent_reward = "0.06"
+            self.opponent_reward = "0.024"
             self.mouse_center_reward = "0.0"
             self.opponent_center_reward = "0.0"
             self.cd_cnt += 1
             mouse1.DeliverReward(Locations.Defect, self.sucker_time)
             mouse2.DeliverReward(Locations.Cooperate, self.temptation_time)
             self.time_to_make_decision = time.time() - self.time_start
+            self.numcompletedtrial += 1
+            self.start_return_time = time.time()
+
 
         elif state == States.M1DM2C:
             # Actions for M1DCM2C state
             self.mouse_choice = "D"
             self.opponent_choice = "C"
-            self.mouse_reward = "0.06"
+            self.mouse_reward = "0.024"
             self.opponent_reward = "0"
             self.mouse_center_reward = "0.0"
             self.opponent_center_reward = "0.0"
@@ -175,19 +160,23 @@ class ExperimentManager:
             mouse1.DeliverReward(Locations.Cooperate, self.temptation_time)
             mouse2.DeliverReward(Locations.Defect, self.sucker_time)
             self.time_to_make_decision = time.time() - self.time_start
+            self.numcompletedtrial += 1
+            self.start_return_time = time.time()
 
         elif state == States.M1DM2D:
             # Actions for M1DM2D state
             self.mouse_choice = "D"
             self.opponent_choice = "D"
-            self.mouse_reward = "0.015"
-            self.opponent_reward = "0.015"
+            self.mouse_reward = "0.006"
+            self.opponent_reward = "0.006"
             self.mouse_center_reward = "0.0"
             self.opponent_center_reward = "0.0"
             self.dd_cnt += 1
             mouse1.DeliverReward(Locations.Defect, self.punishment_time)
             mouse2.DeliverReward(Locations.Defect, self.punishment_time)
             self.time_to_make_decision = time.time() - self.time_start
+            self.numcompletedtrial += 1
+            self.start_return_time = time.time()
 
 
 
@@ -195,7 +184,7 @@ class ExperimentManager:
         elif state == States.TrialCompleted:
             # Increment the trial number counter
 
-            self.numcompletedtrial += 1
+
             self.timestamps = {
                 'Start Time': self.time_start,
                 'Decision Time': self.time_to_make_decision,
@@ -212,6 +201,7 @@ class ExperimentManager:
             self.trial_logger.log_trial_data(self.numcompletedtrial, "Completed Trial", self.opponent_choice,
                                              self.mouse_choice, self.mouse_reward,self.mouse_center_reward, self.opponent_reward,self.opponent_center_reward,
                                              self.time_start, self.time_to_make_decision, self.time_to_return_to_center)
+
 
 
         elif state == States.ReturnTimeOut:
@@ -254,39 +244,19 @@ class ExperimentManager:
         elif state == States.End:
             # Stop recording, finalize logs, show end message, etc.
             self.trial_logger.finalize_logging()
-            #if self.numcompletedtrial>1:
-                #analysis_results = self.data_analyzer.analyze_data()
-                #print(analysis_results)
+            
     def get_data_file_path(self):
         # Retrieve the file path from the TrialLogger instance
         return self.trial_logger.get_csv_file_path()
 
-    def start_streaming_exp(self, experiment_name, num_trial, decision_time, return_time, opponent_type,
-                            opponent1_strategy, opponent2_strategy):
 
-        self.num_trial = num_trial
+    def start_streaming_exp(self, experiment_parameters, mouse1, mouse2, opponent_path):
+        self.trial_logger.start_logging(experiment_parameters.get("mouse_id"), opponent_path)
+        self.event_logger.start_logging(experiment_parameters.get("mouse_id"), opponent_path)
+        self.num_trial = experiment_parameters.get("num_trials")
+        force_end_time = self.num_trial * (experiment_parameters.get("decision_time") + experiment_parameters.get("return_time"))
 
-
-        print("opponent ", opponent_type)
-        print("opponent strategy ", opponent1_strategy)
-
-
-        if opponent_type == OpponentType.MOUSE_MOUSE:
-            mouse1 = MouseMonitor(1, self.videoAnalyser, self.reward_manager)
-            mouse2 = MouseMonitor(2, self.videoAnalyser, self.reward_manager)
-            self.opponent_path = "MOUSE_MOUSE"
-        elif opponent_type == OpponentType.MOUSE_COMPUTER:
-            mouse1 = MouseMonitor(1, self.videoAnalyser, self.reward_manager)
-            mouse2 = FixedStrategyPrisoner(opponent1_strategy, probability=0.8)
-            self.opponent_path = "MOUSE_COMPUTER"
-        else:
-            mouse1 = FixedStrategyPrisoner(opponent1_strategy, probability=0.8)
-            mouse2 = FixedStrategyPrisoner(opponent2_strategy, probability=0.8)
-            self.opponent_path = "COMPUTER_COMPUTER"
-        self.trial_logger.start_logging(self.mouse_number, self.opponent_path)
-        self.event_logger.start_logging(self.mouse_number, self.opponent_path)
-
-        self.stateManager.SetTimeOut(decision_time, return_time)
+        self.stateManager.SetTimeOut(experiment_parameters.get("decision_time"), experiment_parameters.get("return_time"))
 
         currentstate = None
         state_history = []
